5f12470c0767d6ab1e296a533275dd98f6c91862
Chris <Chris@Chris-MacBook-Pro.local>
2022-02-18 00:46:36 +0800

    20220218 finnal

--- src/components/GameBar/GameBar.jsx
+++ src/components/GameBar/GameBar.jsx
@@ -87,14 +87,20 @@ const GameBar = ({ propsMenuGame }) => {
               <p className="location">{item.location}</p>
               <div className="gameTeam">
                 <p className="doFlex">
-                  <img src={iconStyle(item.homeTeam[0])} alt="icon"></img>
+                 <span>
+                 <img src={iconStyle(item.homeTeam[0])} alt="icon"></img>
                   <span>{item.homeTeam[0]}</span>
+                 </span>
+                
 
                   <span>{item.homeTeam[1]}</span>
                 </p>
                 <p className="doFlex">
-                  <img src={iconStyle(item.guestTeam[0])} alt="icon"></img>
+                 <span>
+                 <img src={iconStyle(item.guestTeam[0])} alt="icon"></img>
                   <span>{item.guestTeam[0]}</span>
+                 </span>
+                
                   <span>{item.guestTeam[1]}</span>
                 </p>
               </div>
