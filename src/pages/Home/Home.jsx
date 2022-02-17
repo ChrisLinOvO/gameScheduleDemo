55d4b28deacbab1d5c0a694894ebb4e3cfa9f45b
Chris <Chris@Chris-MacBook-Pro.local>
2022-02-17 18:14:25 +0800

    20220217 修正樣式

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -31,10 +31,13 @@ const Home = () => {
 
   return (
     <div className="home">
+      <div className="headerBox">
       <div className="allow">＜</div>
-      <div className="header">新竹街口攻城獅厲害</div>
+      <div className="header">新竹街口攻城獅</div>
       <div className="title">賽程表</div>
       <div className="text">聯盟戰況</div>
+      </div>
+    
 
       <DateBar propsMenuDate={menuDate} onDateChange={changeDate} />
       <GameBar propsMenuGame={menuGame} />
